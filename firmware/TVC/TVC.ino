4facdadf9c01047b472dc870976b34d78b1fbf4c
washingtonkigan@gmail.com <washingtonkigan@gmail.com>
2021-09-23 16:22:59 +0300

    Update organization

--- firmware/TVC/TVC.ino
+++ firmware/TVC/TVC.ino
@@ -1,49 +1,69 @@
+#include <Servo.h>
 #include <MPU6050_tockn.h>
 #include <Wire.h>
-#include <Servo.h>
+
+MPU6050 mpu6050(Wire);
+
+double kpX = 2;
+double kiX = 5;
+double kdX = 1;
+
+double kpY = 2;
+double kiY = 5;
+double kdY = 1;
+ 
+unsigned long currentTime, previousTime;
+double elapsedTime;
+double errorX, errorY;
+double lastErrorX, lastErrorY;
+double inputX, inputY, outputX, outputY, setPointX, setPointY;
+double cumErrorX, cumErrorY, rateErrorX, rateErrorY;
 
 Servo servoX;
 Servo servoY;
-MPU6050 mpu(Wire);
 
-double kp = 2;
-double ki = 5;
-double kd = 1 ;
-
-unsigned long currentTime, previousTime;
-double elapsedTime;
-double error;
-double lastError;
-double cumError, rateError;
-double setPointX, setPointY;
-double inputX, inputY, outputX, outputY;
- 
 void setup(){
-        servoX.attach(9);
-        servoY.attach(10);
-        Wire.begin();
-        mpu.begin();
-        mpu.calcGyroOffsets(true);
-        setPointX = 0;                          //desired set point for X angle
-        setPointY = 0;                          //desired set point for Y angle
+  servoX.attach(3);
+  servoY.attach(4);
+  Wire.begin();
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets(true);
+  setPointX = 0;//set point at zero degrees
+  setPointY = 0;
+  
 }    
  
 void loop(){
-        inputX = mpu.getAngleX();                //read from rotary encoder connected to A0
-        inputY = mpu.getAngleY();
-        outputX = computePIDX(inputX);
-        outputY = computePIDY(inputY);
-        delay(10);
-        servoX.write(outputX);                //control the motor based on PID value
-        servoY.write(outputY);
- 
+     inputX = analogRead(A0);                //read from rotary encoder connected to A0
+     inputY = analogRead(A1);
+     outputX = computePIDX(inputX);
+     outputY = computePIDY(inputY);
+
+     if(outputX > 180){
+        outputX = 180;
+      }
+
+     if(outputX < 0){
+        outputX = 0;
+      }
+
+     if(outputY > 180){
+        outputY = 180;
+      }
+
+     if(outputY < 0){
+        outputY = 0;
+      }
+      
+     servoX.write(outputX);                //control the motor based on PID value
+     servoY.write(outputY);
 }
  
 double computePIDX(double inp){     
         currentTime = millis();                //get current time
         elapsedTime = (double)(currentTime - previousTime);        //compute time elapsed from previous computation
         
-        error = setPointX - inputX;                                // determine error
+        error = setPointX - inp;                                // determine error
         cumError += error * elapsedTime;                // compute integral
         rateError = (error - lastError)/elapsedTime;   // compute derivative
  
@@ -60,11 +80,11 @@ double computePIDY(double inp){
         currentTime = millis();                //get current time
         elapsedTime = (double)(currentTime - previousTime);        //compute time elapsed from previous computation
         
-        error = setPointY - inputY;                                // determine error
+        error = setPointY - inp;                                // determine error
         cumError += error * elapsedTime;                // compute integral
         rateError = (error - lastError)/elapsedTime;   // compute derivative
  
-        double out = kp*error + ki*cumError + kd*rateError;                //PID output               
+        double out = kpY*error + kiY*cumError + kdY*rateError;                //PID output               
  
         lastError = error;                                //remember current error
         previousTime = currentTime;                        //remember current time
--- firmware/firmware/firmware.ino
+++ firmware/firmware/firmware.ino
@@ -1,69 +1,49 @@
-#include <Servo.h>
 #include <MPU6050_tockn.h>
 #include <Wire.h>
-
-MPU6050 mpu6050(Wire);
-
-double kp = 2;
-double ki = 5;
-double kd = 1;
-
-double kp1 = 2;
-double ki1 = 5;
-double kd1 = 1;
- 
-unsigned long currentTime, previousTime;
-double elapsedTime;
-double errorX, errorY;
-double lastErrorX, lastErrorY;
-double inputX, inputY, outputX, outputY, setPointX, setPointY;
-double cumErrorX, cumErrorY, rateErrorX, rateErrorY;
+#include <Servo.h>
 
 Servo servoX;
 Servo servoY;
+MPU6050 mpu(Wire);
 
+double kp = 2;
+double ki = 5;
+double kd = 1 ;
+
+unsigned long currentTime, previousTime;
+double elapsedTime;
+double error;
+double lastError;
+double cumError, rateError;
+double setPointX, setPointY;
+double inputX, inputY, outputX, outputY;
+ 
 void setup(){
-  servoX.attach(3);
-  servoY.attach(4);
-  Wire.begin();
-  mpu6050.begin();
-  mpu6050.calcGyroOffsets(true);
-  setPointX = 0;//set point at zero degrees
-  setPointY = 0;
-  
+        servoX.attach(5);
+        servoY.attach(6);
+        Wire.begin();
+        mpu.begin();
+        mpu.calcGyroOffsets(true);
+        setPointX = 0;                          //desired set point for X angle
+        setPointY = 0;                          //desired set point for Y angle
 }    
  
 void loop(){
-     inputX = analogRead(A0);                //read from rotary encoder connected to A0
-     inputY = 
-     outputX = computePIDX(inputX);
-     outputY = computePIDY(inputY);
-
-     if(outputX > 180){
-        outputX = 180;
-      }
-
-     if(outputX < 0){
-        outputX = 0;
-      }
-
-     if(outputY > 180){
-        outputY = 180;
-      }
-
-     if(outputY < 0){
-        outputY = 0;
-      }
-      
-     servoX.write(outputX);                //control the motor based on PID value
-     servoY.write(outputY);
+        inputX = mpu.getAngleX();                //read from rotary encoder connected to A0
+        inputY = mpu.getAngleY();
+        outputX = computePIDX(inputX);
+        outputY = computePIDY(inputY);
+        delay(10);
+        servoX.write(outputX);                //control the motor based on PID value
+        servoY.write(outputY);
+ 
 }
  
 double computePIDX(double inp){     
         currentTime = millis();                //get current time
         elapsedTime = (double)(currentTime - previousTime);        //compute time elapsed from previous computation
         
-        error = Setpoint - inp;                                // determine error
+        error = setPointX - inputX;                                // determine error
         cumError += error * elapsedTime;                // compute integral
         rateError = (error - lastError)/elapsedTime;   // compute derivative
  
@@ -80,7 +60,7 @@ double computePIDY(double inp){
         currentTime = millis();                //get current time
         elapsedTime = (double)(currentTime - previousTime);        //compute time elapsed from previous computation
         
-        error = Setpoint - inp;                                // determine error
+        error = setPointY - inputY;                                // determine error
         cumError += error * elapsedTime;                // compute integral
         rateError = (error - lastError)/elapsedTime;   // compute derivative
  
